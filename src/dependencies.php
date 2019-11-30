b13191e9f00e751e929ee908db0f837bd4dbecaf
georginaw <watson.georgina@gmail.com>
2019-11-30 16:05:17 +0000

    added the db connection into the DI container

--- src/dependencies.php
+++ src/dependencies.php
@@ -20,4 +20,10 @@ return function (App $app) {
         return $logger;
     };
 
+    $container['db'] = function() {
+        $db = new PDO('mysql:host=127.0.0.1;dbname=shoppingList', 'root', 'password');
+        $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        return $db;
+    };
+
 };
